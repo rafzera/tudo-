f5a865f8739abff4a7f0d4e0ead7d4eebb93f9c0
Rafzera <rafaelmarquesp19@hotmail.com>
2019-10-23 17:19:34 -0300

    feaT(a): adicionar pagina de about-us

--- a/project/src/App.js
+++ a/project/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PageWrapper from  "./components/PageWrapper"
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Home from "./components/Pages/Home"
+import About from './components/Pages/About'
 
 function App() {
   return (
+    <Router>
       <PageWrapper>
-        <Home/>
+        
+          <Route path="/"  exact component={Home} />
+          <Route path="/about" component={About}/>
+       
       </PageWrapper>
+      </Router>
   );
 }
 
